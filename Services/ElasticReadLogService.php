b31fa03d1834e4e4589c9ae4c63875f3347f57ee
Gabriel <Bordovsky.Gabriel@gmail.com>
2016-06-15 08:17:53 +0200

    AdminActionRenamed

--- Services/ElasticReadLogService.php
+++ Services/ElasticReadLogService.php
@@ -320,7 +320,7 @@ class ElasticReadLogService
     {
         $params = [
             'index' => $this->index,
-            'type' => 'AdminActionLog',
+            'type' => 'EntityActionLog',
             'body' => [
             ]
         ];
